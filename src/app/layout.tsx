f77dfc3630bf01146931de08fdb852cb0d1c37e9
Abhishek Panchgalle <magicabhi1234@gmail.com>
2026-06-22 10:44:30 +0530

    Added dynamic SEO metadata and landing page updates

---
+++ src/app/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+    title: {
+      default: "Online MBA Colleges",
+      template: "%s | Online MBA Colleges",
+    },
+    description:
+      "Compare top Online MBA Universities in India.",
+  };
