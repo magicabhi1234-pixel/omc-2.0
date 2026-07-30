3eedf37cad21713229c722284208a88023a0c66e
Abhishek Panchgalle <magicabhi1234@gmail.com>
2026-07-30 15:49:24 +0530

    select university commit

--- src/components/common/FloatingSelectUniversity.tsx
+++ src/components/common/FloatingSelectUniversity.tsx
@@ -10,13 +10,13 @@ export default function FloatingSelectUniversity() {
   return (
     <OpenPopupButton
       ariaLabel="Select University"
-      className="floating-select-university fixed bottom-4 left-[30px] z-[9990] h-[70px] w-[104px] cursor-pointer overflow-hidden sm:h-[85px] sm:w-[127px] md:h-[100px] md:w-[149px]"
+      className="floating-select-university fixed bottom-4 left-[30px] z-[9990] h-[70px] w-[70px] cursor-pointer overflow-hidden rounded-[50px] md:h-[80px] md:w-[80px]"
     >
       <Image
         src="/select_university.png"
         alt="Select University"
         fill
-        sizes="(max-width: 640px) 104px, (max-width: 768px) 127px, 149px"
+        sizes="(max-width: 767px) 70px, 80px"
         className="object-contain"
       />
     </OpenPopupButton>
